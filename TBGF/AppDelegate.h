8192f3f7fb817701e0adac56be5fa55c15856be1
张晓东 <zhangxiaodong@zhangxiongdembp.lan>
2017-11-22 14:55:06 +0800

    Initial Commit

---
+++ TBGF/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TBGF
+//
+//  Created by 张晓东 on 2017/11/22.
+//  Copyright © 2017年 张晓东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
